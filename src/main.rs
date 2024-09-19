deefefed1d6c31b6f75fbe3ed9a92fa4b7eb629c
Timmy Jose <zoltan.jose@gmail.com>
2024-09-19 22:22:25 +0530

    chore: added readme.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,79 @@
-use std::env;
+use std::{env, sync::Arc};
 
 use actix_cors::Cors;
 use actix_web::{
-    get, http::header::ContentType, post, web, App, HttpResponse, HttpServer, Responder,
-    ResponseError,
+    get,
+    http::{header::ContentType, StatusCode},
+    post, web, App, HttpResponse, HttpServer, Responder, ResponseError,
 };
 use middleware::Middleware;
 use sentry::ClientInitGuard;
 use sentry_tracing::EventFilter;
 use serde::{Deserialize, Serialize};
-use tracing::info;
+use tracing::{error, info};
 use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 mod middleware;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error(transparent)]
-    Actix(#[from] actix_web::Error),
-
     #[error("cannot divide by zero")]
     DivideByZero,
+
+    #[error("SENRTY_DSN is unset")]
+    MissingSentryDsn,
+
+    #[error(transparent)]
+    Actix(#[from] actix_web::Error),
 
     #[error(transparent)]
     DotEnvy(#[from] dotenvy::Error),
 
     #[error(transparent)]
     Io(#[from] std::io::Error),
-
-    #[error("SENRTY_DSN is unset")]
-    MissingSentryDsn,
 }
 
-impl ResponseError for Error {}
+#[derive(Debug)]
+pub struct HTTPError {
+    pub status_code: StatusCode,
+    pub source: Box<dyn std::error::Error>,
+}
+
+impl std::error::Error for HTTPError {}
+
+impl std::fmt::Display for HTTPError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{ {}, {} }}", self.status_code, self.source)
+    }
+}
+
+impl From<Error> for HTTPError {
+    fn from(err: Error) -> Self {
+        let http_error = match err {
+            Error::DivideByZero => HTTPError {
+                status_code: StatusCode::BAD_REQUEST,
+                source: err.into(),
+            },
+            _ => HTTPError {
+                status_code: StatusCode::INTERNAL_SERVER_ERROR,
+                source: err.into(),
+            },
+        };
+
+        sentry::with_scope(
+            |scope| {
+                scope.set_extra("status_code", http_error.status_code.as_u16().into());
+            },
+            || sentry::capture_error(&http_error),
+        );
+
+        http_error
+    }
+}
+
+impl ResponseError for HTTPError {}
+
+pub type HttpResult<T> = std::result::Result<T, HTTPError>;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -50,9 +91,7 @@ async fn mul(x: i32, y: i32) -> Result<i32> {
 
 async fn div(x: i32, y: i32) -> Result<i32> {
     if y == 0 {
-        let err = Error::DivideByZero;
-        sentry::capture_error(&err);
-        Err(err)
+        Err(Error::DivideByZero)
     } else {
         Ok(x + y)
     }
@@ -64,6 +103,17 @@ async fn init_tracing() -> Result<ClientInitGuard> {
         sentry_dsn,
         sentry::ClientOptions {
             release: sentry::release_name!(),
+            before_send: Some(Arc::new(|event| {
+                if let Some(status_code) = event.extra.get("status_code") {
+                    let status_code = status_code.as_u64().unwrap_or(200);
+
+                    if (400..500).contains(&status_code) {
+                        println!("Intercepted a 4xx notification");
+                        return None;
+                    }
+                }
+                Some(event)
+            })),
             ..Default::default()
         },
     ));
@@ -96,8 +146,11 @@ pub struct CalculationResponse {
 
 #[tracing::instrument]
 #[post("/add")]
-async fn handle_add(body: web::Json<CalculationRequest>) -> Result<web::Json<CalculationResponse>> {
+async fn handle_add(
+    body: web::Json<CalculationRequest>,
+) -> HttpResult<web::Json<CalculationResponse>> {
     info!(method = "handle_add", ?body, "adding two numbers together");
+    error!("add");
 
     let x = body.x;
     let y = body.y;
@@ -108,7 +161,9 @@ async fn handle_add(body: web::Json<CalculationRequest>) -> Result<web::Json<Cal
 
 #[tracing::instrument]
 #[post("/sub")]
-async fn handle_sub(body: web::Json<CalculationRequest>) -> Result<web::Json<CalculationResponse>> {
+async fn handle_sub(
+    body: web::Json<CalculationRequest>,
+) -> HttpResult<web::Json<CalculationResponse>> {
     info!(
         method = "handle_sub",
         ?body,
@@ -124,7 +179,9 @@ async fn handle_sub(body: web::Json<CalculationRequest>) -> Result<web::Json<Cal
 
 #[tracing::instrument]
 #[post("/mul")]
-async fn handle_mul(body: web::Json<CalculationRequest>) -> Result<web::Json<CalculationResponse>> {
+async fn handle_mul(
+    body: web::Json<CalculationRequest>,
+) -> HttpResult<web::Json<CalculationResponse>> {
     info!(method = "handle_mul", ?body, "multiplying two numbers");
 
     let x = body.x;
@@ -136,7 +193,9 @@ async fn handle_mul(body: web::Json<CalculationRequest>) -> Result<web::Json<Cal
 
 #[tracing::instrument]
 #[post("/div")]
-async fn handle_div(body: web::Json<CalculationRequest>) -> Result<web::Json<CalculationResponse>> {
+async fn handle_div(
+    body: web::Json<CalculationRequest>,
+) -> HttpResult<web::Json<CalculationResponse>> {
     info!(method = "handle_div", ?body, "Dividing a number by another");
 
     let x = body.x;
